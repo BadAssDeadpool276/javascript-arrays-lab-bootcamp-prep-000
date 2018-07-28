656037a5c2e7471f53714060809bfc00338fa25d
Wyatt Massey <wyattisboss276@gmail.com>
2018-07-28 06:31:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your functions and code here
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+}
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+}
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+}
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+}
+function appendKitten(name){
+  return kittens.concat(name);
+}
+function prependKitten(name){
+  return concat(name,kittens);
+}
